939eaf3ee2c1b581ffb8a4e2059e0a0f9281e606
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-17 16:04:03 -0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+
+import Home from './pages/Home.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '*', element: <PageNotFound /> }
+    ]
+
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
