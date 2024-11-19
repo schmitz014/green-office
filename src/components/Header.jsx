b23dfd15c70155a1f1d889f580ff655727aabdae
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-19 20:23:18 -0300

    fix: correct reponsive value

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,12 +14,12 @@ export default function Header() {
             <header className="bg-medium-green text-color-white font-poppins">
                 <nav className="flex items-center justify-between mx-5">
                     <div className="flex items-center py-5 gap-5">
-                        <img src="/public/Logo.svg" alt="Logo do Green Office" className="w-10 md:w-20 lg:w-28"/>
+                        <img src="/public/Logo.svg" alt="Logo do Green Office" className="w-10 md:w-20 xl:w-28"/>
                         <h1 className="text-2xl md:text-5xl xl:text-6xl font-bold">GREENOFFICE</h1>
                     </div>
                     <div>
                         {/* Desktop */}
-                        <ul className="lg:flex text-2xl hidden lg:gap-5">
+                        <ul className="xl:flex text-2xl hidden xl:gap-5">
                             <li className="hover:italic hover:font-semibold hover:scale-105 transition-all"><NavLink to="/">Home</NavLink></li>
                             <li className="hover:italic hover:font-semibold hover:scale-105 transition-all"><NavLink to="/necessidade">Necessidade</NavLink></li>
                             <li className="hover:italic hover:font-semibold hover:scale-105 transition-all"><NavLink to="/energia">Energia</NavLink></li>
@@ -28,7 +28,7 @@ export default function Header() {
                         </ul>
         
                         {/* Burger */}
-                        <div className="lg:hidden flex items-center">
+                        <div className="xl:hidden flex items-center">
                             <button className="space-y-1 md:space-y-3" onClick={openMobileMenu}>
                                 <span className="block w-6 h-1 md:w-20 md:h-2 bg-color-white"></span>
                                 <span className="block w-6 h-1 md:w-20 md:h-2 bg-color-white"></span>
@@ -39,15 +39,15 @@ export default function Header() {
                 </nav>
                 {/* Mobile */}
                 {MobileMenu && (
-                    <div className="flex flex-col items-center bg-medium-green text-color-white gap-y-5 p-5 text-xl">
+                    <div className="flex flex-col items-center bg-medium-green text-color-white gap-y-5 p-5 text-xl md:text-2xl">
                         <NavLink to="/">Home</NavLink>
-                        <span className="block w-full h-1 md:w-20 md:h-2 bg-color-white"></span>
+                        <span className="block w-full h-1 md:h-2 bg-color-white"></span>
                         <NavLink to="/necessidade">Necessidade</NavLink>
-                        <span className="block w-full h-1 md:w-20 md:h-2 bg-color-white"></span>
+                        <span className="block w-full h-1 md:h-2 bg-color-white"></span>
                         <NavLink to="/energia">Energia</NavLink>
-                        <span className="block w-full h-1 md:w-20 md:h-2 bg-color-white"></span>
+                        <span className="block w-full h-1 md:h-2 bg-color-white"></span>
                         <NavLink to="/tecnologia">Tecnologia</NavLink>
-                        <span className="block w-full h-1 md:w-20 md:h-2 bg-color-white"></span>
+                        <span className="block w-full h-1 md:h-2 bg-color-white"></span>
                         <NavLink to="/colabore">Colabore</NavLink>
                     </div>
                 )}
