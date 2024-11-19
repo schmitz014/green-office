9dd752e1f6c0efd8c123626dfbdc65479e9e6935
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-19 20:59:40 -0300

    feat: config page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,15 @@
+import { BsEmojiDizzyFill } from "react-icons/bs";
+
 export default function PageNotFound() {
     return (
-        <>
-            <h1>404</h1>
-            <p>Page not found</p>
-        </>
+        <div className="text-center font-poppins m-10">
+            <div>
+                <div className="flex items-center justify-center gap-5 text-dark-green text-9xl">
+                    <BsEmojiDizzyFill />
+                    <h1 className="font-bold">404</h1>
+                </div>
+                <h2 className="text-3xl font-semibold text-dark-green mt-10">Página não encontrada</h2>
+            </div>
+        </div>
     );
 }
