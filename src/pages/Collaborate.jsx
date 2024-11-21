1814284043069e0e8cbf6007200d3214d62276b5
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-21 16:35:14 -0300

    feat: add imgs for carousel

--- src/pages/Collaborate.jsx
+++ src/pages/Collaborate.jsx
@@ -4,6 +4,11 @@ export default function Collaborate() {
     return(
         <>
             <Title title="Colabore" />
+            <div className="flex justify-center items-center m-5 font-poppins">
+                <p className='text-xl font-medium text-center text-wrap px-10'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veniam cupiditate reprehenderit enim obcaecati fugit unde rem quia delectus, fuga repudiandae, tempore adipisci iusto? Fugit voluptatibus nobis at repellendus ut!
+                </p>
+            </div>
         </>
     )
 }
