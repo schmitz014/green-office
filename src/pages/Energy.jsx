bd45487ab9335f31e5d60da2212054d21017c9b5
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-21 12:34:48 -0300

    feat: add energytype component

--- src/pages/Energy.jsx
+++ src/pages/Energy.jsx
@@ -9,7 +9,13 @@ export default function Energy() {
                 <p className="text-center text-xl px-16">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste sapiente odit velit blanditiis quibusdam? Ea assumenda nam pariatur dignissimos maiores?
                 </p>
-                <EnergyType img={"/public/img/Energies/solar.jpg"} alt={"Energia Solar"} subtitle={"Solar"} link={""}/>
+                <div className="flex">
+                    <EnergyType img={"/public/img/Energies/solar.jpg"} alt={"Foto Energia Solar"} subtitle={"Solar"} link={""}/>
+                    <EnergyType img={"/public/img/Energies/wind.jpg"} alt={"Foto Energia Eólica"} subtitle={"Eólica"} link={""}/>
+                    <EnergyType img={"/public/img/Energies/biomass.jpg"} alt={"Foto Energia Biomassa"} subtitle={"Biomassa"} link={""}/>
+                    <EnergyType img={"/public/img/Energies/geothermal.jpg"} alt={"Foto Energia Geotérmica"} subtitle={"Geotérmica"} link={""}/>
+                    <EnergyType img={"/public/img/Energies/hydroeletric.jpg"} alt={"Foto Energia Hidrelétrica"} subtitle={"Hidrelétrica"} link={""}/>
+                </div>
             </div>
         </>
     )
