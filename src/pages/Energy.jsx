f7d802f363e4face3861dc16b0ae50331546a4c8
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-21 08:22:53 -0300

    feat: add text

--- src/pages/Energy.jsx
+++ src/pages/Energy.jsx
@@ -4,7 +4,11 @@ export default function Energy() {
     return (
         <>
             <Title title="Energia" />
-            
+            <div className="font-poppins font-semibold">
+                <p className="text-center text-xl px-16">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste sapiente odit velit blanditiis quibusdam? Ea assumenda nam pariatur dignissimos maiores?
+                </p>
+            </div>
         </>
     )
 }
