3090b2380245e49fc84bcd14726ed9d1f10fdfaa
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-21 15:45:29 -0300

    fix: adjust import name

--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -1,4 +1,4 @@
-import TechCarouselCard from "../components/TechCard";
+import TechCard from "../components/TechCard";
 import TitleDark from "../components/TitleDark";
 
 export default function Tech() {
@@ -6,31 +6,31 @@ export default function Tech() {
         <>
             <TitleDark title="Tecnologia e Inovação" />
             <div className="flex justify-center items-center flex-col xl:flex-row m-5 gap-10">
-                <TechCarouselCard 
+                <TechCard 
                     img="/img/Energies/solar.jpg" 
                     alt="Painel de energia solar em um dia ensolarado" 
                     title="Energia Solar" 
                     benefits="Energia renovável, sustentável e econômica." 
                 />
-                <TechCarouselCard 
+                <TechCard 
                     img="/img/Energies/wind.jpg" 
                     alt="Turbina eólica em um campo" 
                     title="Energia Eólica" 
                     benefits="Energia limpa e renovável, sem emissão de gases."
                 />
-                <TechCarouselCard 
+                <TechCard 
                     img="/img/Energies/biomass.jpg" 
                     alt="Biomassa de madeira" 
                     title="Energia de Biomassa" 
                     benefits="Energia renovável, limpa e de baixo custo."
                 />
-                <TechCarouselCard 
+                <TechCard 
                     img="/img/Energies/hydroeletric.jpg" 
                     alt="Barragem de uma hidrelétrica" 
                     title="Energia Hidrelétrica" 
                     benefits="Energia limpa, renovável e de baixo custo."
                 />
-                <TechCarouselCard 
+                <TechCard 
                     img="/img/Energies/geothermal.jpg" 
                     alt="Usina geotérmica" 
                     title="Energia Geotérmica" 
